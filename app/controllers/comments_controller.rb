24a7ca32991f5b18c019146e59b8a165eb797c49
kangkyu <kangkyu1111@gmail.com>
2015-01-04 20:12:44 -0800

    finish add comment form under article show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,20 @@ class CommentsController < ApplicationController
   
   def index
   end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.new(comment_params)
+    if @comment.save
+      redirect_to article_url(@article)
+    else
+      render :back
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :title)
+  end
 end
