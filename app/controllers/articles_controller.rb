adb4413c36c3081b5611b1818b42c38ee631680c
kangkyu <kangkyu1111@gmail.com>
2015-03-20 11:21:01 -0700

    clean view templates a little up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    @comment = Comment.new
+    @comments = @article.comments
   end
 
   def new
