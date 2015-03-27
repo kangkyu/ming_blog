064ea3fe9238007e387765d68ff42c2ead4e1464
kangkyu <kangkyu1111@gmail.com>
2015-03-26 22:57:26 -0700

    edit application controller to add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   private 
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
+    # @current_user ||= User.where(id: session[:user_id]).take
+    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
@@ -16,5 +18,11 @@ class ApplicationController < ActionController::Base
       redirect_to root_url, alert: "Please log in or sign up"
     end
   end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  helper_method :logged_in?
 
 end
