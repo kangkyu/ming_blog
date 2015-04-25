575115767c732d1c8c64c5a6ea91c77f4144abbb
kangkyu <kangkyu1111@gmail.com>
2015-04-25 14:20:29 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+articles = Article.create!([
+{
+  title: "Lorem ipsum",
+  body: "dolor sit amet, ea sit detracto iracundia euripidis. Mei eu minim reprimique, his justo theophrastus ne. Vel at soleat moderatius appellantur, nam an accusam vituperatoribus. Aperiri persius detraxit pro ex, ea vim vivendo fabellas, duis omnis nulla mei ex.
+
+Has animal mnesarchum no, cum omittam signiferumque ut. Cu facer euripidis eos, ad officiis expetendis nam. Vim in suas prima atqui. Qui an cibo prompta lucilius. No amet probatus vix, his saepe utinam instructior no.
+
+Eam docendi honestatis temporibus no. Quando audire vivendum te nec, sint laudem expetenda eam no, iusto graece salutandi qui an. Usu ea ridens meliore, ea nullam dicunt doctus duo. Vim ipsum efficiendi an. Ut cum hinc meis omittantur, sensibus vulputate complectitur ex duo.
+
+Eros tation singulis vel eu. Ex nec fabulas legimus accumsan. No duis exerci reprehendunt eum, malis dicat ullum mea id. Ei option dolorum duo.
+
+Vis in putant fabellas, eos inani possit impetus an. Vim corpora probatus convenire in, ex eam volumus sapientem conceptam. Pri te possit insolens pericula. Eu vulputate deseruisse ius. Veniam voluptatum neglegentur vel id. Duo no assum epicurei, te vocibus pertinax imperdiet nam."
+},
+{
+  title: "Hello, world!",
+  body: "Extend Bootstrap's grid system with the thumbnail component to easily display grids of images, videos, text, and more."
+}])
+
+Comment.create!(title: "Yes!", text: "Go for it!", article: articles.first)
+User.create!(name: "kang-kyu lee", email: "lee@kangkyu.com", user_name: "kangkyu", password: "1111", password_confirmation: "1111")
