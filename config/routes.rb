23f7ae4f404fecb9fbce242df027621675d3e976
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-11-22 08:34:17 -0800

    remove trailing empty spaces

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  
+
   root 'articles#index'
 
   resource :session
-  
+
   resources :users
 
   get '/signup' => 'users#new', as: 'signup'
