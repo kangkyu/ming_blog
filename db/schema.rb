bd2511d17e8d6fe066ad3f6233ca7d99fe973ce0
kangkyu <kangkyu1111@gmail.com>
2021-12-27 18:00:08 -0800

    rails app:update - update configs

--- db/schema.rb
+++ db/schema.rb
@@ -1,45 +1,44 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119054416) do
+ActiveRecord::Schema.define(version: 2021_12_28_012224) do
 
-  create_table "articles", force: true do |t|
-    t.string   "title"
-    t.text     "body"
+  create_table "articles", force: :cascade do |t|
+    t.string "title", limit: 255
+    t.text "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "comments", force: true do |t|
-    t.text     "text"
-    t.string   "title"
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.string "title", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "article_id"
+    t.integer "article_id"
   end
 
-  create_table "names", force: true do |t|
-    t.string   "email"
-    t.string   "password_digest"
+  create_table "names", force: :cascade do |t|
+    t.string "email", limit: 255
+    t.string "password_digest", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "user_name"
-    t.string   "password_digest"
+  create_table "users", force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "email", limit: 255
+    t.string "user_name", limit: 255
+    t.string "password_digest", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
